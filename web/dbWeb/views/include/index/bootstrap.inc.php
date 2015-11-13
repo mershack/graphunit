08ab54d039b7700e13a5bc154dc283f7f99a5a48
dyami004 <dyami004@fiu.edu>
2015-11-12 23:37:06 -0500

    latest working with java servlets

--- web/dbWeb/views/include/index/bootstrap.inc.php
+++ web/dbWeb/views/include/index/bootstrap.inc.php
@@ -3,4 +3,5 @@
 <script src="js/jquery-2.1.4.min.js"></script> 
 <script src="js/js.cookie.js" type="text/javascript"></script>
 <script src="js/viewerUpload.js" type="text/javascript"></script>
+<script src="js/datasetUpload.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js"></script>
